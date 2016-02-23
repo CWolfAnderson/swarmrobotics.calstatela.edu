b99572fcf93190170a4b7c7b3869fdb6c46b76d3
Christoph Anderson <CWolfAnderson@gmail.com>
2016-02-22 23:04:38 -0800

    small changes

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(function() {
+
+  // $("body").keydown(function(event) {
+  //
+  //   console.log(event.which);
+  //   if (event.which === 38) { // up
+  //     $("#rover").css("top", top-5);
+  //   } else if (event.which === 40) { // down
+  //     $("#rover").css("top", +5);
+  //   } else if (event.which === 37) { // left
+  //     $("#rover").css("left", -5);
+  //   } else if (event.which === 39) { // right
+  //     $("#rover").css("left", +5);
+  //   }
+  // });
+
+  $(document).mousemove(function(e){
+    $("#rover").stop().animate({left:e.pageX, top:e.pageY});
+});
+
+});
